39ac5c8025f70fe4bfbfb173d579a776607e855e
Anshakov Kirill Igorevich <kkk.rbhbkk@bk.ru>
2023-10-17 02:31:23 +0300

    do pycodestyle

--- test/test_CalcQuartile.py
+++ test/test_CalcQuartile.py
@@ -11,35 +11,35 @@ class TestCalcRating:
     @pytest.fixture()
     def input_data(self) -> tuple[RatingsType, float, StudentIn]:
         rating: DataType = {
-            'Иванов Иван Иванович': 82.0,
-            'Петров Петр Петрович': 83.66666666666667,
-            'Сидоров Сидор Сидорович': 72.33333333333333,
-            'Васильев Василий Васильевич': 93.33333333333333
+            "Иванов Иван Иванович": 82.0,
+            "Петров Петр Петрович": 83.66666666666667,
+            "Сидоров Сидор Сидорович": 72.33333333333333,
+            "Васильев Василий Васильевич": 93.33333333333333,
         }
 
         lastquartile = 86.08333333333334
 
-        rating_scores: StudentIn = ['Васильев Василий Васильевич']
+        rating_scores: StudentIn = ["Васильев Василий Васильевич"]
 
         return rating, lastquartile, rating_scores
 
-    def test_calc_last_quartile(self, input_data: tuple[DataType,
-                                                    float, StudentIn]) -> None:
+    def test_calc_last_quartile(
+        self, input_data: tuple[DataType, float, StudentIn]
+    ) -> None:
         calc_rating = CalcQuartile(input_data[0])
         assert input_data[0] == calc_rating.rating
 
-    def test_calc_quartile(self, input_data: tuple[DataType, float, StudentIn]) -> None:
+    def test_calc_quartile(self, input_data: tuple[DataType, float, StudentIn]
+                           ) -> None:
 
         rating = CalcQuartile(input_data[0]).calc_last_quartile()
-        assert pytest.approx(rating,
-                             abs=0.001) == input_data[1]
+        assert pytest.approx(rating, abs=0.001) == input_data[1]
 
-    def test_shows_calc_quartile(self, input_data: tuple[DataType,
-                                                        float, StudentIn]) -> None:
+    def test_shows_calc_quartile(
+        self, input_data: tuple[DataType, float, StudentIn]
+    ) -> None:
 
-        rating_scores: StudentIn = CalcQuartile(input_data[0]).\
-            show_all_students_last_quartile(input_data[1])
+        rating_scores: StudentIn = CalcQuartile(
+            input_data[0]
+        ).show_all_students_last_quartile(input_data[1])
         assert rating_scores == input_data[2]
-
-
-
